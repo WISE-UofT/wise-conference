969e313505959d628d9aee558bc8d7d15fed9722
percy-ai <startdash71@gmail.com>
2025-08-25 13:33:03 -0400

    update scrolling speed

--- js/main.js
+++ js/main.js
@@ -1,279 +1,199 @@
-jQuery(document).ready(function($) {
-
-    // Add a dot for each slide
-    var numSlides = $('.carousel-slide').length;
-    for (var i = 0; i < numSlides; i++) {
-        $('.carousel-dots').append('<div class="carousel-dot"></div>');
-    }
-
-    // Set the first dot as active
-    $('.carousel-dot:first').addClass('active');
-
-    // Function to go to a specific slide
+jQuery(function ($) {
+    // ------- Fast smooth scroll settings -------
+    const FAST_SCROLL_MS = 300;
+    $.easing.easeInOutQuart = function (x, t, b, c, d) {
+      if ((t /= d / 2) < 1) return (c / 2) * t * t * t * t + b;
+      return (-c / 2) * ((t -= 2) * t * t * t - 2) + b;
+    };
+  
+    // ------- Carousel dots -------
+    const $slides = $(".carousel-slide");
+    const $dotsWrap = $(".carousel-dots");
+    const numSlides = $slides.length;
+  
+    for (let i = 0; i < numSlides; i++) $dotsWrap.append('<div class="carousel-dot"></div>');
+    $(".carousel-dot:first").addClass("active");
+  
     function goToSlide(index) {
-        $('.carousel-slide').hide();
-        $('.carousel-slide:eq(' + index + ')').show();
-        $('.carousel-dot').removeClass('active');
-        $('.carousel-dot:eq(' + index + ')').addClass('active');
+      $slides.hide().eq(index).show();
+      $(".carousel-dot").removeClass("active").eq(index).addClass("active");
     }
-
-    // Handle dot click event using event delegation
-    $('.carousel-dots').on('click', '.carousel-dot', function () {
-        clearInterval(slideInterval); // Stop the timer
-        var index = $(this).index();
-        goToSlide(index);
-        // Restart the timer
-        slideInterval = setInterval(nextSlide, 3000); // Change 3000 to the desired interval in milliseconds
-    });
-
-    // Function to go to the next slide
+  
     function nextSlide() {
-        var currentIndex = $('.carousel-slide:visible').index();
-        var nextIndex = (currentIndex + 1) % numSlides;
-        goToSlide(nextIndex);
+      const currentIndex = $slides.filter(":visible").index();
+      const nextIndex = (currentIndex + 1) % numSlides;
+      goToSlide(nextIndex);
     }
-
-    // Set a timer to automatically advance the carousel
-    var slideInterval = setInterval(nextSlide, 3000); // Change 3000 to the desired interval in milliseconds
-
-    // Back to top button
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
+  
+    let slideInterval = setInterval(nextSlide, 3000);
+    $dotsWrap.on("click", ".carousel-dot", function () {
+      clearInterval(slideInterval);
+      goToSlide($(this).index());
+      slideInterval = setInterval(nextSlide, 3000);
     });
-    $('.back-to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1500, 'easeInOutExpo');
-        return false;
+  
+    // ------- Back to top -------
+    const $backToTop = $(".back-to-top");
+    $(window).on("scroll", function () {
+      $(this).scrollTop() > 100 ? $backToTop.fadeIn("slow") : $backToTop.fadeOut("slow");
     });
-
-    // Preloader
-    $(window).on('load', function() {
-        if ($('#preloader').length) {
-            $('#preloader').delay(100).fadeOut('slow', function() {
-                $(this).remove();
-            });
-        }
+    $backToTop.on("click", function () {
+      $("html, body").animate({ scrollTop: 0 }, FAST_SCROLL_MS, "easeInOutQuart");
+      return false;
     });
-
-    // Header fixed on scroll
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $('#header').addClass('header-scrolled');
-        } else {
-            $('#header').removeClass('header-scrolled');
-        }
+  
+    // ------- Preloader -------
+    $(window).on("load", function () {
+      const $pre = $("#preloader");
+      if ($pre.length) $pre.delay(100).fadeOut("slow", function () { $(this).remove(); });
     });
-
-    if ($(window).scrollTop() > 100) {
-        $('#header').addClass('header-scrolled');
+  
+    // ------- Header on scroll -------
+    const $header = $("#header");
+    function updateHeader() {
+      $(window).scrollTop() > 100 ? $header.addClass("header-scrolled") : $header.removeClass("header-scrolled");
     }
-
-    // Real view height for mobile devices
+    $(window).on("scroll", updateHeader);
+    updateHeader();
+  
+    // ------- Mobile real VH -------
     if (window.matchMedia("(max-width: 767px)").matches) {
-        $('#intro').css({
-            height: $(window).height()
-        });
+      $("#intro").css({ height: $(window).height() });
     }
-
-    // Initiate the wowjs animation library
-    new WOW().init();
-
-    // Initialize Venobox
-    $('.venobox').venobox({
-        bgcolor: '',
-        overlayColor: 'rgba(6, 12, 34, 0.85)',
-        closeBackground: '',
-        closeColor: '#fff'
+  
+    // ------- Plugins -------
+    if (typeof WOW === "function") new WOW().init();
+  
+    $(".venobox").venobox({
+      bgcolor: "",
+      overlayColor: "rgba(6, 12, 34, 0.85)",
+      closeBackground: "",
+      closeColor: "#fff",
     });
-
-    // Initiate superfish on nav menu
-    $('.nav-menu').superfish({
-        animation: {
-            opacity: 'show'
-        },
-        speed: 400
-    });
-
-    // Mobile Navigation
-    if ($('#nav-menu-container').length) {
-        var $mobile_nav = $('#nav-menu-container').clone().prop({
-            id: 'mobile-nav'
-        });
-        $mobile_nav.find('> ul').attr({
-            'class': '',
-            'id': ''
-        });
-        $('body').append($mobile_nav);
-        $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
-        $('body').append('<div id="mobile-body-overly"></div>');
-        $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
-
-        $(document).on('click', '.menu-has-children i', function(e) {
-            $(this).next().toggleClass('menu-item-active');
-            $(this).nextAll('ul').eq(0).slideToggle();
-            $(this).toggleClass("fa-chevron-up fa-chevron-down");
-        });
-
-        $(document).on('click', '#mobile-nav-toggle', function(e) {
-            $('body').toggleClass('mobile-nav-active');
-            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-            $('#mobile-body-overly').toggle();
-        });
-
-        $(document).click(function(e) {
-            var container = $("#mobile-nav, #mobile-nav-toggle");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
-                if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-                    $('#mobile-body-overly').fadeOut();
-                }
-            }
-        });
+  
+    $(".nav-menu").superfish({ animation: { opacity: "show" }, speed: 400 });
+  
+    // ------- Mobile Navigation -------
+    if ($("#nav-menu-container").length) {
+      const $mobileNav = $("#nav-menu-container").clone().prop({ id: "mobile-nav" });
+      $mobileNav.find("> ul").attr({ class: "", id: "" });
+      $("body").append($mobileNav)
+               .prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>')
+               .append('<div id="mobile-body-overly"></div>');
+      $("#mobile-nav").find(".menu-has-children").prepend('<i class="fa fa-chevron-down"></i>');
+  
+      $(document).on("click", ".menu-has-children i", function () {
+        $(this).next().toggleClass("menu-item-active").nextAll("ul").eq(0).slideToggle();
+        $(this).toggleClass("fa-chevron-up fa-chevron-down");
+      });
+  
+      $(document).on("click", "#mobile-nav-toggle", function () {
+        $("body").toggleClass("mobile-nav-active");
+        $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+        $("#mobile-body-overly").toggle();
+      });
+  
+      $(document).on("click", function (e) {
+        const $container = $("#mobile-nav, #mobile-nav-toggle");
+        if (!$container.is(e.target) && $container.has(e.target).length === 0 && $("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+          $("#mobile-body-overly").fadeOut();
+        }
+      });
     } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-        $("#mobile-nav, #mobile-nav-toggle").hide();
+      $("#mobile-nav, #mobile-nav-toggle").hide();
     }
-
-    // Smooth scroll for the menu and links with .scrollto classes
-    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            if (target.length) {
-                var top_space = 0;
-
-                if ($('#header').length) {
-                    top_space = $('#header').outerHeight();
-
-                    if (!$('#header').hasClass('header-fixed')) {
-                        top_space = top_space - 20;
-                    }
-                }
-
-                $('html, body').animate({
-                    scrollTop: target.offset().top - top_space
-                }, 1500, 'easeInOutExpo');
-
-                if ($(this).parents('.nav-menu').length) {
-                    $('.nav-menu .menu-active').removeClass('menu-active');
-                    $(this).closest('li').addClass('menu-active');
-                }
-
-                if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-                    $('#mobile-body-overly').fadeOut();
-                }
-                return false;
-            }
+  
+    // ------- Smooth scroll (FAST only) -------
+    $(".nav-menu a, #mobile-nav a, .scrollto").on("click", function () {
+      if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+        const $target = $(this.hash);
+        if ($target.length) {
+          let topSpace = 0;
+          if ($header.length) {
+            topSpace = $header.outerHeight();
+            if (!$header.hasClass("header-fixed")) topSpace -= 20;
+          }
+  
+          $("html, body").stop(true).animate(
+            { scrollTop: $target.offset().top - topSpace },
+            FAST_SCROLL_MS,
+            "easeInOutQuart"
+          );
+  
+          if ($(this).parents(".nav-menu").length) {
+            $(".nav-menu .menu-active").removeClass("menu-active");
+            $(this).closest("li").addClass("menu-active");
+          }
+  
+          if ($("body").hasClass("mobile-nav-active")) {
+            $("body").removeClass("mobile-nav-active");
+            $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+            $("#mobile-body-overly").fadeOut();
+          }
+          return false;
         }
+      }
     });
-
-    // gallery carousel (uses the Owl Carousel library)
+  
+    // ------- Gallery carousel (Owl) -------
     $(".gallery-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: false,
-        center: true,
-        responsive: {
-            0: {
-                items: 0
-            },
-            250: {
-                items: 1
-            },
-            500: {
-                items: 2
-            },
-            600: {
-                items: 3
-            }
-        }
+      autoplay: true,
+      dots: true,
+      loop: false,
+      center: true,
+      responsive: { 0: { items: 0 }, 250: { items: 1 }, 500: { items: 2 }, 600: { items: 3 } },
     });
-
-    // Buy tickets select the ticket type on click
-    $('#buy-ticket-modal').on('show.bs.modal', function(event) {
-        var button = $(event.relatedTarget);
-        var ticketType = button.data('ticket-type');
-        var modal = $(this);
-        modal.find('#ticket-type').val(ticketType);
-    })
-
-    /* ======= Countdown ========= */
-	// set the date we're counting down to
-    var target_date = new Date("Jan 25, 2025").getTime();
-     
-    // variables for time units
-    var days, hours, minutes, seconds;
-     
-    // get tag element
-    var countdown =  document.getElementById("countdown-box");
-    var days_span = document.createElement("SPAN");
-    days_span.className = 'days';
-    countdown.appendChild(days_span);
-    var hours_span = document.createElement("SPAN");
-    hours_span.className = 'hours';
-    countdown.appendChild(hours_span);
-    var minutes_span = document.createElement("SPAN");
-    minutes_span.className = 'minutes';
-    countdown.appendChild(minutes_span);
-    var secs_span = document.createElement("SPAN");
-    secs_span.className = 'secs';
-    countdown.appendChild(secs_span);
-     
-    // update the tag with id "countdown" every 1 second
-    setInterval(function () {
-     
-        // find the amount of "seconds" between now and target
-        var current_date = new Date().getTime();
-        var seconds_left = (target_date - current_date) / 1000;
-     
-        // do some time calculations
-        days = parseInt(seconds_left / 86400);
-        seconds_left = seconds_left % 86400;
-         
-        hours = parseInt(seconds_left / 3600);
-        seconds_left = seconds_left % 3600;
-         
-        minutes = parseInt(seconds_left / 60);
-        seconds = parseInt(seconds_left % 60);
-         
-        // format countdown string + set tag value.
-        days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Days</span>';
-        hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
-        minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
-        secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Secs</span>'; 
-     
-    }, 1000);
-
-    /**Testimonials slider**/
-    new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-    delay: 5000,
-    disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true
-    },
-    breakpoints: {
-    320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-    },
-
-    1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-    }
-    }
+  
+    // ------- Buy tickets modal -------
+    $("#buy-ticket-modal").on("show.bs.modal", function (event) {
+      const ticketType = $(event.relatedTarget).data("ticket-type");
+      $(this).find("#ticket-type").val(ticketType);
     });
-});
+  
+    // ------- Countdown -------
+    (function initCountdown() {
+      const targetDate = new Date("Jan 25, 2025").getTime();
+      const box = document.getElementById("countdown-box");
+      if (!box) return;
+  
+      const daysEl = document.createElement("SPAN");
+      daysEl.className = "days";
+      const hoursEl = document.createElement("SPAN");
+      hoursEl.className = "hours";
+      const minsEl = document.createElement("SPAN");
+      minsEl.className = "minutes";
+      const secsEl = document.createElement("SPAN");
+      secsEl.className = "secs";
+      box.append(daysEl, hoursEl, minsEl, secsEl);
+  
+      setInterval(function () {
+        const now = Date.now();
+        let secondsLeft = (targetDate - now) / 1000;
+  
+        const days = parseInt(secondsLeft / 86400, 10); secondsLeft %= 86400;
+        const hours = parseInt(secondsLeft / 3600, 10); secondsLeft %= 3600;
+        const minutes = parseInt(secondsLeft / 60, 10);
+        const seconds = parseInt(secondsLeft % 60, 10);
+  
+        daysEl.innerHTML = `<span class="number">${days}</span><span class="unit">Days</span>`;
+        hoursEl.innerHTML = `<span class="number">${hours}</span><span class="unit">Hrs</span>`;
+        minsEl.innerHTML = `<span class="number">${minutes}</span><span class="unit">Mins</span>`;
+        secsEl.innerHTML = `<span class="number">${seconds}</span><span class="unit">Secs</span>`;
+      }, 1000);
+    })();
+  
+    // ------- Testimonials (Swiper) -------
+    if (typeof Swiper === "function") {
+      new Swiper(".testimonials-slider", {
+        speed: 600,
+        loop: true,
+        autoplay: { delay: 5000, disableOnInteraction: false },
+        slidesPerView: "auto",
+        pagination: { el: ".swiper-pagination", type: "bullets", clickable: true },
+        breakpoints: { 320: { slidesPerView: 1, spaceBetween: 20 }, 1200: { slidesPerView: 3, spaceBetween: 20 } },
+      });
+    }
+  });
+  
