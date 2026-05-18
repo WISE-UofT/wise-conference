2fc3f30d0b2a489189da576412db7ce0df6928a4
Angela Zheng <angelazheng96@gmail.com>
2026-05-18 19:11:23 -0400

    fix discount content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const discounts = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.md", base: "./src/content/discounts" }),
   schema: z.object({
     school: z.string(),
   }),
